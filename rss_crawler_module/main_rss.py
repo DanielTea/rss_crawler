4be4012fdf0643857a3788168a05eee171ac7cac
Daniel T <info@danieltremer.com>
2024-04-21 09:11:57 +0300

    Refactor RSS crawling process for efficiency
    - Removed commented-out code
    - Utilized ThreadPoolExecutor for concurrent processing
    - Improved logging and error handling

--- rss_crawler_module/main_rss.py
+++ rss_crawler_module/main_rss.py
@@ -41,30 +41,30 @@ def start_crawl_with_urls():
 
     from utilityDbScript import insert_rss_article
 
-    # links = load_rss_links_from_database()
+    links = load_rss_links_from_database()
 
-    # from concurrent.futures import ThreadPoolExecutor
+    from concurrent.futures import ThreadPoolExecutor
 
-    # total_crawled_links = 0
-    # rss_reader = RSSReader()
+    total_crawled_links = 0
+    rss_reader = RSSReader()
 
-    # def process_url(url):
-    #     nonlocal total_crawled_links
-    #     print(f"Starting crawl for {url}...")
+    def process_url(url):
+        nonlocal total_crawled_links
+        print(f"Starting crawl for {url}...")
         
-    #     crawled_entries = rss_reader._fetch_rss_entries(url)
-    #     if crawled_entries:
-    #         print(f"Found {len(crawled_entries)} entries in {url}")
-    #         total_crawled_links += len(crawled_entries)
-    #         for entry in crawled_entries:
-    #             print(f"Inserting crawled entry {entry['link']} into the database...")
-    #             if entry:
-    #                 insert_rss_article(entry['publisher'], entry['title'], entry['link'], entry['published'], entry['language'], entry['crawl_date'])
+        crawled_entries = rss_reader._fetch_rss_entries(url)
+        if crawled_entries:
+            print(f"Found {len(crawled_entries)} entries in {url}")
+            total_crawled_links += len(crawled_entries)
+            for entry in crawled_entries:
+                print(f"Inserting crawled entry {entry['link']} into the database...")
+                if entry:
+                    insert_rss_article(entry['publisher'], entry['title'], entry['link'], entry['published'], entry['language'], entry['crawl_date'])
 
-    # with ThreadPoolExecutor(max_workers=100) as executor:
-    #     executor.map(process_url, links)
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        executor.map(process_url, links)
 
-    # print(f"Total crawled entries: {total_crawled_links}")
+    print(f"Total crawled entries: {total_crawled_links}")
 
 if __name__ == "__main__":
     start_crawl_with_urls()
